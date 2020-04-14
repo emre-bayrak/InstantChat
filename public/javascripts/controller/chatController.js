76410e29977a039a54807b7d100f1830b5ef2239
Emre <bayrakemre89@gmail.com>
2020-04-14 17:35:32 +0100

    Rooms upsert method init

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -2,14 +2,18 @@ app.controller('chatController', ['$scope', ($scope) => {
     $scope.onlineUsers = [];
     $scope.activeTab = 2;
 
-    $scope.changeTab = tab => {
-        $scope.activeTab = tab;
-    };
-
     const socket = io.connect("http://67659045.ngrok.io/");
-
     socket.on('onlineList', users => {
         $scope.onlineUsers = users;
         $scope.$apply();
     });
+
+    $scope.newRoom = () => {
+        let randomName = Math.random().toString(36).substring(7);
+        socket.emit('newRoom', randomName);
+    };
+
+    $scope.changeTab = tab => {
+        $scope.activeTab = tab;
+    };
 }]);
