1473779a1da47195dba30dae634f8401047b20eb
Emre <bayrakemre89@gmail.com>
2020-04-15 00:02:59 +0100

    redis updated

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,8 +1,6 @@
 const redis = require('redis');
 
 const getClient = () => redis.createClient({
-        /*host: process.env.REDIS_URI,
-        port: process.env.REDIS_PORT*/
         url: 'redis://h:pb3f73d0f54eff7a4533a360a9fdf7edfe663debcb66c245c3fd21c8f7d10b392@ec2-52-21-150-124.compute-1.amazonaws.com:27379'
 });
 
