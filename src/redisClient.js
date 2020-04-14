8654dacd7bc91620f3ea2676f1df187318848a93
Emre <bayrakemre89@gmail.com>
2020-04-15 00:47:09 +0100

    redis cloud

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,5 +1,5 @@
 const redis = require('redis');
 
-const getClient = () => redis.createClient(process.env.REDIS_URL);
+const getClient = () => redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
 module.exports.getClient = getClient;
--- src/socketApi.js
+++ src/socketApi.js
@@ -16,13 +16,7 @@ io.use(socketAuthorization);
 
 // Redis Adapter
 const redisAdapter = require('socket.io-redis');
-io.adapter(redisAdapter({ 
-    host: 'ec2-52-21-150-124.compute-1.amazonaws.com',
-    port: 27379,
-    user: 'h',
-    password: 'pb3f73d0f54eff7a4533a360a9fdf7edfe663debcb66c245c3fd21c8f7d10b392' 
-    /*url: 'redis://h:pb3f73d0f54eff7a4533a360a9fdf7edfe663debcb66c245c3fd21c8f7d10b392@ec2-52-21-150-124.compute-1.amazonaws.com:27379'*/
-}));
+io.adapter(redisAdapter(process.env.REDISCLOUD_URL, {no_ready_check: true}));
 
 io.on('connection', socket => {
     console.log('A user logged in with name ' + socket.request.user.firstName);
