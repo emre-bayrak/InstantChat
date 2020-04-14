a8d2a004698233f8d6e378a814075d0d0145d41d
Emre <bayrakemre89@gmail.com>
2020-04-14 16:05:52 +0100

    Redis remove method init

--- src/socketApi.js
+++ src/socketApi.js
@@ -23,6 +23,10 @@ io.on('connection', socket => {
     console.log('A user logged in with name ' + socket.request.user.firstName);
 
     Users.upsert(socket.id, socket.request.user);
+
+    socket.on('disconnect', () => {
+        Users.remove(socket.request.user.googleId);
+    });
 });
 
 module.exports = socketApi;
