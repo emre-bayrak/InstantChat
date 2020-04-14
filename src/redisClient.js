2b50a37b8f00f63eb1d08feb5529d95efb04560c
Emre <bayrakemre89@gmail.com>
2020-04-15 00:37:42 +0100

    redis trials

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,11 +1,5 @@
 const redis = require('redis');
 
-const getClient = () => redis.createClient({
-        host: 'ec2-52-21-150-124.compute-1.amazonaws.com',
-        port: 27379,
-        user: 'h',
-        password: 'pb3f73d0f54eff7a4533a360a9fdf7edfe663debcb66c245c3fd21c8f7d10b392' 
-        /*url: 'redis://h:pb3f73d0f54eff7a4533a360a9fdf7edfe663debcb66c245c3fd21c8f7d10b392@ec2-52-21-150-124.compute-1.amazonaws.com:27379'*/
-});
+const getClient = () => redis.createClient(process.env.REDIS_URL);
 
 module.exports.getClient = getClient;
