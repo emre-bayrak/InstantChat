c1ccb5a935edb098233d0c4cad280b57d6ed766f
Emre <bayrakemre89@gmail.com>
2020-04-14 17:22:34 +0100

    OnlineUsers list on UI

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,4 +1,5 @@
 app.controller('chatController', ['$scope', ($scope) => {
+    $scope.onlineUsers = [];
     $scope.activeTab = 2;
 
     $scope.changeTab = tab => {
@@ -6,4 +7,9 @@ app.controller('chatController', ['$scope', ($scope) => {
     };
 
     const socket = io.connect("http://67659045.ngrok.io/");
+
+    socket.on('onlineList', users => {
+        $scope.onlineUsers = users;
+        $scope.$apply();
+    });
 }]);
