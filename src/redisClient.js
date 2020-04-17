7dd45bb57715d98ae6184693dfdc7dfcc212d5e5
Emre <bayrakemre89@gmail.com>
2020-04-17 12:22:52 +0100

    redis trialll

--- src/redisClient.js
+++ src/redisClient.js
@@ -3,7 +3,8 @@ const redis = require('redis');
 const getClient = () => redis.createClient({
     host: 'redis-15572.c12.us-east-1-4.ec2.cloud.redislabs.com',
     port: 15572,
-    auth_pass: 'x8n9ombgHqNQZAvcG0L9lvBpWiQw42nn'
-}, {no_ready_check: true});
+    auth_pass: 'x8n9ombgHqNQZAvcG0L9lvBpWiQw42nn',
+    no_ready_check: true
+});
 
 module.exports.getClient = getClient;
