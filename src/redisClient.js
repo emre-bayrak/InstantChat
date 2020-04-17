a04ba7b7a9b9a6a963afe2792710bf734a081810
Emre <bayrakemre89@gmail.com>
2020-04-17 12:40:36 +0100

    redis

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,5 +1,5 @@
 const redis = require('redis');
 
-const getClient = () => redis.createClient(15572,'redis-15572.c12.us-east-1-4.ec2.cloud.redislabs.com', {no_ready_check: true});
+const getClient = () => /*redis.createClient(15572,'redis-15572.c12.us-east-1-4.ec2.cloud.redislabs.com', {no_ready_check: true})*/true;
 
 module.exports.getClient = getClient;
